bc1a63fc1b43e9182cc4c4b2b8d661686d19f3f4
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-06-13 20:49:15 -0700

    added new fetch for multiple data for cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,25 @@
-var longitude = -118.2436596;
-var latitude = 34.0522265;
 var token =
   "pk.eyJ1Ijoia2lsbGJlZXZvbDIiLCJhIjoiY2twdndpanZ0MHltZjJ2b2lhNmp3Y2k3cCJ9.ZxtIFLMKwODb0Cp2ZfIcDw";
 
-// var mymap = L.map("mapid").setView([latitude, longitude], 10);
-
-// L.tileLayer(
-//   `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${token}`,
-//   {
-//     attribution:
-//       'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: "mapbox/streets-v11",
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: token,
-//   }
-// ).addTo(mymap);
-
-// var circle = L.circle([latitude, longitude], {
-//   color: "red",
-//   fillColor: "#f03",
-//   fillOpacity: 0.5,
-//   radius: 5000,
-// }).addTo(mymap);
-
+var myMap = L.map("mapid");
 const submit = document.getElementById("submit");
 
 submit.addEventListener("click", searchApi);
 
-function searchApi(e) {
-  e.preventDefault();
+var globalCities = [];
+
+fetch(`https://www.trackcorona.live/api/cities`)
+  .then((response) => response.json())
+  .then((response) => {
+    var data = response.data;
+
+    for (let i = 0; i < data.length; i++) {
+      globalCities.push(data[i]);
+    }
+  });
+
+function searchApi(event) {
+  event.preventDefault();
   const search = document.getElementById("search").value;
   console.log(search);
 
@@ -41,7 +30,7 @@ function searchApi(e) {
 
       const latitude = response.data[0].latitude;
       const longitude = response.data[0].longitude;
-      var mymap = L.map("mapid").setView([latitude, longitude], 10);
+      myMap.setView([latitude, longitude], 8);
 
       L.tileLayer(
         `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${token}`,
@@ -54,13 +43,20 @@ function searchApi(e) {
           zoomOffset: -1,
           accessToken: token,
         }
-      ).addTo(mymap);
+      ).addTo(myMap);
 
       var circle = L.circle([latitude, longitude], {
         color: "red",
         fillColor: "#f03",
         fillOpacity: 0.5,
-        radius: 5000,
-      }).addTo(mymap);
+        radius: 100000,
+      }).addTo(myMap);
+
+      circle.bindPopup(
+        "<span class='stateName'>" +
+          response.data[0].location +
+          "</span><br>Confirmed cases " +
+          response.data[0].confirmed
+      );
     });
 }
